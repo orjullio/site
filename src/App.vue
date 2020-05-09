747075a23cf930fc3044c3dbc0c3c7ffd87281fe
Rodrigo Júlio Costa  Silva <13160078+rjullio@users.noreply.github.com>
2020-05-09 17:12:59 -0300

    download

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{title}}</h1>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function(){
+    return{
+      title:"Rodrigo" 
+    }
   }
+  
 }
 </script>
 
@@ -21,7 +24,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
