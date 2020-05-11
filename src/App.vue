8ebc41edd7f0cd05a293bef1ca656868ce51c3b0
Rodrigo Júlio Costa  Silva <13160078+rjullio@users.noreply.github.com>
2020-05-11 13:21:04 -0300

    Inclusão de componentes.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a class="navbar-brand" href="#">Rodrigo Júlio</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Início<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Formação</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Feramentas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contato</a>
-          </li>
-        </ul>
-      </div>
-  </nav>
+    <barra-nav></barra-nav>
     
   </div>
 </template>
 
 <script>
-
+import Nav from './components/nav/Nav.vue';
 
 export default {
   name: 'App',
+  components: {
+    'barra-nav': Nav
+  },
   data: function(){
     return{
       title:"Rodrigo" 
@@ -42,10 +24,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+  
 }
 </style>
